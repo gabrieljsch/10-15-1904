9d3f8ad45c9361c230ed37e8b706231c1b544b46
Kyle <kylearcher96@sbcglobal.net>
2018-01-14 12:59:33 -0500

    Add random pathfinding at stops

--- move_directly.py
+++ move_directly.py
@@ -16,7 +16,7 @@ def move_directly(gc, unit,location_to_go):
     map_loc = unit.location.map_location()
     #get direction to objective
     direction_to_go = map_loc.direction_to(location_to_go)
-
+    directions = list(bc.Direction)
 
     #check cooldown
     if unit.movement_heat() < 10:
@@ -24,5 +24,20 @@ def move_directly(gc, unit,location_to_go):
         try:
             if gc.can_move(unit.id,direction_to_go) == True:
                 gc.move_robot(unit.id, direction_to_go)
+
+            else:
+                try:
+                    counter = 0
+                    new_dir = random.choice(bc.Direction)
+                    while gc.can_move(unit.id, new_dir) == False:
+                        new_dir = random.choice(bc.Direction)
+                        counter +=1
+                        if counter >8:
+                            print("cant move")
+                    if gc.can_move(unit.id,new_dir) == True:
+                        gc.move_robot(unit.id, new_dir)
+                except:
+                    traceback.print_exc()
+
         except:
             traceback.print_exc()
