d89f522771764e17b98815a3d20f76ad341ba716
gabrieljsch <gschneid@mit.edu>
2018-01-15 13:03:34 -0500

    Adding tasks folder with first task, harvest then build

---
+++ tasks/harvest.py
@@ -0,0 +1,36 @@
+import battlecode as bc
+
+directions = list(bc.Direction)
+
+class Harvest_then_build(Task):
+
+	def __init__(self, worker):
+		self.mined = 0
+		self.factory_built = False
+		self.worker = worker
+
+	def execute(self):
+		if self.mined != 50:
+			for direction in directions:
+				if gc.can_harvest(worker.id, direction):
+					gc.harvest(worker.id, direction)
+					self.mined += worker_harvest_amount()
+					return
+		else:
+			for direction in directions:
+				if gc.can_build(worker.id, direction):
+					gc.build(worker.id, direction)
+					self.factory_built = True
+					return
+
+
+	def is_done(self):
+		return self.factory_built
+
+
+
+
+
+
+
+
