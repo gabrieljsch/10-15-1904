e08c1c8cbf2b84539d43e5ba0e1653c3baa62cdb
Kyle <kylearcher96@sbcglobal.net>
2018-01-15 12:44:32 -0500

    Moved tasking tool to main file

--- main.py
+++ main.py
@@ -1,12 +1,36 @@
 # buncha imports or whatever
-
+import battlecode as bc
+import random
+import sys
+import traceback
 # hardcode some tasks assigned to workers:
 task1 = ExampleTask(a, b, c)
 worker1 = Worker(example_unit)
 worker1.assign(task1)
 
+
+
+
+
+# init_game
+print("pystarting")
+# A GameController is the main type that you talk to the game with.
+# Its constructor will connect to a running game.
+gc = bc.GameController()
+
+print("pystarted")
+#set seed
+random.seed(8)
+
+
+
 # main loop:
 while True:
+
+
+    #run on turn one init
+    if gc.round() == 1:
+        
 
     for worker in workers:
         worker.work()
---
+++ on_first_turn.py
@@ -0,0 +1,7 @@
+import battlecode as bc
+import random
+import sys
+import traceback
+
+def on_turn_one():
+    
