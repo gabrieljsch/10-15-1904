88292ce28c2ec4e0887fa0f2d9e7b9228da70ff9
Joshua Tomazin <joshua.tomazin@gmail.com>
2018-01-18 16:43:02 -0500

    Write a bunch of nav code
    Still WIP

--- nav/nav.py
+++ nav/nav.py
@@ -1,4 +1,4 @@
-from collections import namedtuple
+from collections import deque, namedtuple
 import heapq
 
 from battlecode import Direction
@@ -23,7 +23,9 @@ class PriorityQueue:
         heapq.heappush(self.elements, (priority, item))
     
     def get(self):
-        return heapq.heappop(self.elements)[1]
+        el = heapq.heappop(self.elements)
+        
+        return el[1]
 
 def a_star_search(graph, start, goal):
     frontier = PriorityQueue()
@@ -33,22 +35,47 @@ def a_star_search(graph, start, goal):
     came_from[start] = None
     cost_so_far[start] = 0
     final_node = None
-    
+
+    # print([(el[0], el[1].location.x, el[1].location.y, el[1].turn)
+    #        for el in frontier.elements])
     while not frontier.empty():
         current = frontier.get()
+        # print('popped:', current)
+        # print(current.turn)
         
         if graph.success(current, goal):
             final_node = current
             break
-        
-        for next in graph.neighbors(current):
+
+        new_elements = []
+
+
+        neighbors = graph.neighbors(current)
+        # print('neighbors:', neighbors)
+        for next in neighbors:
             new_cost = cost_so_far[current] + graph.cost(current, next)
             if next not in cost_so_far or new_cost < cost_so_far[next]:
                 cost_so_far[next] = new_cost
-                priority = new_cost + heuristic(goal, next)
+                priority = new_cost + graph.heuristic(goal, next)
+                # print(priority, '=', new_cost, '+', graph.heuristic(goal, next))
                 frontier.put(next, priority)
+                new_elements.append((priority, next))
                 came_from[next] = current
-    
+            # else:
+            #     print('here it is')
+            #     print(next)
+            #     print(next)
+            #     print('cost so far:', [(el.location.x, el.location.y, el.turn)
+            #                             for el in cost_so_far.keys()])
+
+        # print('new elements:', [(el[0], el[1].location.x, el[1].location.y, el[1].turn,
+        #                          el[1].heat)
+        #                         for el in new_elements])
+
+        # print('queue:', [(el[0], el[1].location.x, el[1].location.y, el[1].turn)
+        #                  for el in frontier.elements])
+        # input()
+                
     return came_from, final_node #, cost_so_far
 
 def reconstruct_path(came_from, start, end):
@@ -62,15 +89,44 @@ def reconstruct_path(came_from, start, end):
     return path
 
 ### Graph Implementation ###################################
-SearchNode = namedtuple('SearchNode', ['location', 'turn', 'unit', 'heat'])
+class SearchNode(namedtuple('SearchNode', ['location', 'turn', 'unit', 'heat'])):
+    def __lt__(self, other):
+        if self.turn > other.turn:
+            return False
+        elif self.location.x > other.location.x:
+            return False
+        elif self.location.y > other.location.y:
+            return False
+        elif self.heat > other.heat:
+            return False
+        elif self.unit >= other.unit:
+            return False
+        else:
+            return True
 
+    def __eq__(self, other):
+        if not isinstance(other, SearchNode):
+            return False
+        return (self.location.x == other.location.x and
+                self.location.y == other.location.y and
+                self.turn == other.turn and
+                self.unit == other.unit and
+                self.heat == other.heat)
+
+    def __hash__(self):
+        return hash(self.location.x ^
+                    self.location.y ^
+                    self.turn ^
+                    self.unit ^
+                    self.heat)
+        
 class SearchGraph():
 
     def __init__(self, gc, nav):
         self.gc = gc
         self.nav = nav
-        self.occupations = {} # turn to set of occupied (x, y) tuple
-                              # locations
+        self.__occupations = {} # mapping of turn -> set of occupied
+                                # (x, y) tuple locations
 
     def neighbors(self, node):
         dont_move = SearchNode(node.location, node.turn+1, node.unit,
@@ -85,66 +141,93 @@ class SearchGraph():
                           Direction.Southeast,
                           Direction.Southwest,
                           Direction.West]
-            moves = [SearchNode(node.location.add(d),
-                                node.turn + 1,
-                                node.unit,
-                                node.heat + node.unit.movement_cooldown() - 10)
-                     for d in directions
-                     if self.__is_clear(node)]
-            return moves + [dont_move]
+            def neighbor(d):
+                return SearchNode(node.location.add(d),
+                                  node.turn + 1,
+                                  node.unit,
+                                  (node.heat +
+                                   self.gc.unit(node.unit).movement_cooldown() - 10))
+            moves = [neighbor(d) for d in directions]
+            filtered_moves = [m for m in moves if self.__is_clear(m)]
+            # moves = [SearchNode(node.location.add(d),
+            #                     node.turn + 1,
+            #                     node.unit,
+            #                     (node.heat +
+            #                      self.gc.unit(node.unit).movement_cooldown() - 10))
+            #          for d in directions]
+            # print('all moves:', moves)
+            # filtered_moves = [m for m in moves if self.__is_clear(m)]
+            # print('filtered moves:', filtered_moves)
+            
+            return filtered_moves + [dont_move]
         else:
+            #print('dont move:', dont_move)
             return [dont_move]
 
     def __is_clear(self, node):
         '''True iff the given node is not occupied by another unit.'''
         def check_location(location):
-            if self.gc.is_occupiable(node.location):
+            if self.gc.is_occupiable(location):
+                # print('is occupiable')
                 return True
             else:
+                # print('checking parametrics')
                 # could be occupiable later if unit is moving
                 unit = self.gc.sense_unit_at_location(location)
                 if unit:
-                    return unit.id in nav.traveling_units
+                    return unit in self.nav.traveling_units.keys()
                 else:
-                    print("This message should happen -- delete it later if it does")
+                    #print("This message should happen -- delete it later if it does")
                     return False
         
         return (check_location(node.location)
                 and ((node.location.x, node.location.y) not in
-                     self.occupations[node.turn]))
+                     self.occupations(node.turn)))
+
+    def occupations(self, turn):
+        # manage access to turns not in __occupations
+        if turn not in self.__occupations:
+            return set()
+        return self.occupations(turn)
 
     def add_route(self, route):
         for node in route:
-            if not self.occupations[node.turn]:
-                self.occupations[node.turn] = set()
+            if node.turn not in self.__occupations.keys():
+                self.__occupations[node.turn] = set()
             
-            count = len(self.occupations[node.turn])
-            self.occupations[node.turn].add((node.location.x,
-                                             node.location.y))
-            if len(self.occupations[node.turn]) == count:
+            count = len(self.__occupations[node.turn])
+            self.__occupations[node.turn].add((node.location.x,
+                                               node.location.y))
+            if len(self.occupations(node.turn)) == count:
                 print("Routed through an occupied tile!",
                       "Something's wrong with pathfinding.")
 
     def clear_route(self, route):
         for node in route:
-            self.occupations[node.turn].remove((node.location.x, node.location.y))
+            self.occupations(node.turn).remove((node.location.x, node.location.y))
 
     def cost(self, node1, node2):
+        if node2.turn - node1.turn < 0:
+            print("freak out!")
         return node2.turn - node1.turn
 
-    def heuristic(self, node1, node2):
-        x1 = node1.location.x
-        y1 = node1.location.y
+    def heuristic(self, goal, node2):
+        x1 = goal.x
+        y1 = goal.y
         x2 = node2.location.x
         y2 = node2.location.y
-        return abs(x1 - x2) + abs(y1 - y2)
+
+        h = abs(x1 - x2) + abs(y1 - y2)
+        # print(x2, y2, '->', h)
+        return h
 
     def success(self, node, goal):
+        
         # TODO
         true_success = (node.location.x == goal.x and
                         node.location.y == goal.y)
         if true_success:
-            return true
+            return True
         else:
             #if not self.__is_clear(go)
             #if not self.gc.is_occupiable(goal.location):
@@ -185,21 +268,36 @@ class Navigator:
         '''Moves all units towards their destinations.'''
         units_to_move = [u for u in self.traveling_units
                          if self.gc.unit(u).is_move_ready()]
-        # figure out which units can and can't move, 
-        self.traveling_units = None
+        for id, route in self.traveling_units.items():
+            unit = self.gc.unit(id)
+            if unit.is_move_ready():
+                move = route.popleft()
+                dir = unit.location.map_location().direction_to(move)
+                print(dir)
+                if self.gc.can_move(id, dir):
+                    print("moving unit", id)
+                    self.gc.move_robot(id, dir)
+                else:
+                    print("Unit", id, "is blocked! Rerouting...")
+                    self.traveling_units[id] = self.__find_route(id, route[-1])
 
     # probably only useful for testing
     def still_navigating(self):
-        return not self.traveling_units
+        return not not self.traveling_units.keys()
     
     def __find_route(self, unit_id, destination):
         unit = self.gc.unit(unit_id)
-        start_loc = unit.location
+        start_loc = unit.location.map_location()
         start = SearchNode(start_loc, self.gc.round(), unit_id, unit.movement_heat())
         goal = destination
-        came_from = a_star_search(self.graph, start, goal)
+        came_from, end = a_star_search(self.graph, start, goal)
         path = reconstruct_path(came_from, start, end)
-        print(path)
+        path = deque([node.location for node in path])
+        # print([(l.x, l.y) for l in path])
+        # self.traveling_units[unit_id] = path
+        # print([(l.x, l.y) for l in path])
+        # print(self.traveling_units)
+        return path
         
 ### Backup Navigation ######################################
 # TODO Idea is to keep track of total movement of units and fall back
--- nav/test_nav.py
+++ nav/test_nav.py
@@ -1,10 +1,68 @@
 from collections import namedtuple
 
 from nav import Navigator
-from battlecode import Direction
+from battlecode import Direction, Location
+
+directions = [Direction.East,
+              Direction.North,
+              Direction.Northeast,
+              Direction.Northwest,
+              Direction.South,
+              Direction.Southeast,
+              Direction.Southwest,
+              Direction.West,
+              Direction.Center]
+
+def dir_to_delta(dir):
+    if dir is Direction.East:
+        return (0, 1)
+    elif dir is Direction.North:
+        return (-1, 0)
+    elif dir is Direction.Northeast:
+        return (-1, 1)
+    elif dir is Direction.Northwest:
+        return (-1, -1)
+    elif dir is Direction.South:
+        return (1, 0)
+    elif dir is Direction.Southeast:
+        return (1, 1)
+    elif dir is Direction.Southwest:
+        return (1, -1)
+    elif dir is Direction.West:
+        return (0, -1)
+    elif dir is Direction.Center:
+        return (0, 0)
+    else:
+        print("Direction not recognized!")
 
 # Mocks
-Location = namedtuple('Location', ['x', 'y'])
+MapLocationBase = namedtuple('Location', ['x', 'y'])
+# def location_add(x, y, d):
+#     dx, dy = dir_to_delta(d)
+#     return MapLocationBase(x + dx, y + dy, lambda d: location_add(x+dx, y+dy, d))
+    
+# MapLocation = lambda x, y: MapLocationBase(x, y, lambda d: location_add(x, y, d))
+class MapLocation(MapLocationBase):
+    def add(self, d):
+        dx, dy = dir_to_delta(d)
+        return MapLocation(self.x + dx, self.y + dy)
+
+    def distance_squared_to(self, other):
+        x1, y1 = self.x, self.y
+        x2, y2 = other.x, other.y
+        return (x2 - x1)*(x2 - x1) + (y2 - y1)*(y2 - y1)
+    
+    def direction_to(self, other):
+        cands = [(self.add(d), d) for d in directions]
+        print([c[0].distance_squared_to(other) for c in cands])
+        return min(cands, key = lambda c: c[0].distance_squared_to(other))[1]
+
+class Location:
+    def __init__(self, x, y):
+        self.mloc = MapLocation(x, y)
+
+    def map_location(self):
+        return self.mloc
 
 class Unit:
     def __init__(self, id, location):
@@ -12,6 +70,9 @@ class Unit:
         self.location = location
         self.heat = 0
 
+    def movement_cooldown(self):
+        return 20
+        
     def movement_heat(self):
         return self.heat
         
@@ -23,7 +84,7 @@ def loc_to_coord(loc):
 
 def coord_to_loc(coord):
     x, y = coord
-    return Location(x, y)
+    return MapLocation(x, y)
 
 ### Model of the world #####################################
 class World:
@@ -42,26 +103,27 @@ class World:
         self.goal = coord_to_loc(coord)
 
     def __add_unit(self, id, coord):
-        self.units[id] = Unit(id, coord_to_loc(coord))
+        x, y = coord
+        self.units[id] = Unit(id, Location(x, y))
 
     def unit(self, id):
         return self.units[id]
         
     def move_robot(self, id, direction):
         unit = self.units[id]
-        x, y = loc_to_coord(unit.location)
-        dx, dy = self.__dir_to_delta(dir)
+        x, y = loc_to_coord(unit.location.map_location())
+        dx, dy = dir_to_delta(dir)
         new_coord = (x + dx, y + dy)
         if self.is_occupiable(new_coord):
-            unit.location = coord_to_loc(new_coord)
-            unit.heat += 20
+            unit.location.mloc = coord_to_loc(new_coord)
+            unit.heat += unit.movement_cooldown()
         else:
             raise Exception("Robot " + str(id) + " can't move to " +
                             str(new_coord) + "!")
     
     def sense_unit_at_location(self, location):
         matching_unit = [id for id in self.units.keys() if
-                         loc_to_coord(self.units[id].location)
+                         loc_to_coord(self.units[id].location.map_location())
                          == (location.x, location.y)]
         if matching_unit:
             return matching_unit[0]
@@ -70,8 +132,19 @@ class World:
 
     def is_occupiable(self, location):
         coord = (location.x, location.y)
-        return (coord in self.obstacles
-                or coord in [loc_to_coord(u.location) for u in self.units.values()])
+        return (location.x < self.height and
+                location.y < self.width and
+                coord not in self.obstacles and
+                coord not in [loc_to_coord(u.location.map_location())
+                              for u in self.units.values()])
+
+    def can_move(self, id, dir):
+        if dir is Direction.Center:
+            return True
+        unit = self.unit(id)
+        r = self.is_occupiable(unit.location.map_location().add(dir))
+        print(r)
+        return r
 
     def end_turn(self):
         self.rnd += 1
@@ -106,38 +179,16 @@ class World:
 
         return height
 
-    def __dir_to_delta(dir):
-        if dir is Direction.East:
-            return (-1, 0)
-        elif dir is Direction.North:
-            return (0, -1)
-        elif dir is Direction.Northeast:
-            return (-1, -1)
-        elif dir is Direction.Northwest:
-            return (1, -1)
-        elif dir is Direction.South:
-            return (0, 1)
-        elif dir is Direction.Southeast:
-            return (-1, 1)
-        elif dir is Direction.Southwest:
-            return (1, 1)
-        elif dir is Direction.West:
-            return (1, 0)
-        elif dir is Direction.Center:
-            return (0, 0)
-        else:
-            print("Direction not recognized!")
-
     def to_string(self):
         '''Give a printed representation of the world at this point'''
         out = [['.' for i in range(self.width)] for j in range(self.height)]
         # write all data structures to output list:
         for id, unit in self.units.items():
-            x,y = loc_to_coord(unit.location)
+            x, y = loc_to_coord(unit.location.map_location())
             out[x][y] = str(id)
-        for (x, y) in self.obstacles:
+        for x, y in self.obstacles:
             out[x][y] = '#'
-        (x, y) = self.goal
+        x, y = self.goal.x, self.goal.y
         out[x][y] = '*' # TODO multiple goals
 
         # put spaces so it's pretty:
@@ -167,6 +218,7 @@ while nav.still_navigating():
     nav.move_units()
     print(ez_world.to_string())
     ez_world.end_turn()
+    input()
 
 
 simple_blocked ="""
